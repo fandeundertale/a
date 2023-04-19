1df6d002695ea134599d6dbccb8fff5e2fb51f58
fandeundertale <131223076+fandeundertale@users.noreply.github.com>
2023-04-18 20:39:59 -0600

    Add files via upload

---
+++ sck.js
@@ -0,0 +1,9 @@
+function spanelem() {
+    window.alert("Este es un elemento SPAN");
+    document.getElementById("audio").play();
+  }
+  
+  function divelem() {
+    window.alert("Este es un elemento DIV");
+    document.getElementById("audio").pause();
+  }
